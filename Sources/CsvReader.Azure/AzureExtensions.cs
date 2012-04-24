d361310a85c573c34379f3480e1c89c11a480a7c
Mike Stall <mikestall@hotmail.com>
2012-04-23 22:31:20 -0700

    add xml doc comments for azure extension

--- Sources/CsvReader.Azure/AzureExtensions.cs
+++ Sources/CsvReader.Azure/AzureExtensions.cs
@@ -11,12 +11,27 @@ namespace DataAccess
 {
     public static class DataTableAzureExtensions
     {
+        /// <summary>
+        /// Read a data table from azure blob. This will read the entire blob into memory and return a mutable data table.
+        /// </summary>
+        /// <param name="builder">builder</param>
+        /// <param name="account">azure acount</param>
+        /// <param name="containerName">conatiner name</param>
+        /// <param name="blobName">blob name</param>
+        /// <returns>in-memory mutable datatable from blob</returns>
         public static MutableDataTable ReadFromAzureBlob(this DataTableBuilder builder, CloudStorageAccount account, string containerName, string blobName)
         {
             CloudBlobContainer container = GetContainer(account, containerName);
             return ReadFromAzureBlob(builder, container, blobName);
         }
 
+        /// <summary>
+        /// Read a data table from azure blob. This will read the entire blob into memory and return a mutable data table.
+        /// </summary>
+        /// <param name="builder">builder</param>
+        /// <param name="container">conatiner</param>
+        /// <param name="blobName">blob name</param>
+        /// <returns>in-memory mutable datatable from blob</returns>
         public static MutableDataTable ReadFromAzureBlob(this DataTableBuilder builder, CloudBlobContainer container, string blobName)
         {            
             CloudBlob blob = container.GetBlobReference(blobName);
@@ -36,18 +51,24 @@ namespace DataAccess
         }
 
         /// <summary>
-        /// Save the data table to the given azure blob
+        /// Save the data table to the given azure blob. This will overwrite an existing blob.
         /// </summary>
-        /// <param name="table"></param>
-        /// <param name="account"></param>
-        /// <param name="containerName"></param>
-        /// <param name="blobName"></param>
+        /// <param name="table">instance of table to save</param>
+        /// <param name="account">azure acount</param>
+        /// <param name="containerName">conatiner name</param>
+        /// <param name="blobName">blob name</param>
         public static void SaveToAzureBlob(this DataTable table, CloudStorageAccount account, string containerName, string blobName)
         {
             CloudBlobContainer container = GetContainer(account, containerName);
             SaveToAzureBlob(table, container, blobName);
         }
 
+        /// <summary>
+        /// Save the data table to the given azure blob. This will overwrite an existing blob.
+        /// </summary>
+        /// <param name="table">instance of table to save</param>
+        /// <param name="container">conatiner</param>
+        /// <param name="blobName">blob name</param>
         public static void SaveToAzureBlob(this DataTable table, CloudBlobContainer container, string blobName)
         {
             var blob = container.GetBlobReference(blobName);
