fb3b5a41ef6c75cea2a6d365b6c3743a54fc50a3
Mike Stall <mikestall@hotmail.com>
2012-04-23 20:12:29 -0700

    Check ExcelCsvReader into solution.

--- Sources/DataTableTests/FlattenTests.cs
+++ Sources/DataTableTests/FlattenTests.cs
@@ -6,7 +6,6 @@ using System;
 namespace DataTableTests
 {
     // Test flatten function. Used when converting IEnumerable<T> to a table.
-    // $$$ Add these
     public class FlattenTests
     {
         [Fact]
